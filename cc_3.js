81636fe088643eaa60a476d7567f552cc54d1d6d
Sophia Dolan <25srdolan@gmail.com>
2025-01-30 17:45:02 -0500

    Task 2 - Modifying Customer Orders.

--- cc_3.js
+++ cc_3.js
@@ -6,3 +6,12 @@ console.log("Product prices:", prices); //logging prices
 
 // task 2: Modifying Customer Orders Scenario: 
 
+let orders = [2,4,6,8,10]; // order numbers
+orders[2] = orders[2]+5; // adding 5 to the third order
+
+let total = orders.reduce((sum, orders)=>sum + orders, 0);
+//calc the total of the orders 
+
+console.log("My orders:", orders);
+console.log("The total of my orders:", total);
+

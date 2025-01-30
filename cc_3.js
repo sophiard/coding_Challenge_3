011dd2a41c7249fb6bef7994fc6bdd61166b753d
Sophia Dolan <25srdolan@gmail.com>
2025-01-30 17:22:16 -0500

    Task 1 - Product Price Management.

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+// task 1: : Product Price Management Scenario
+let prices = [190,777,678,908,607]; //setting my orginal prices 
+prices.push(808); //adding a price 
+prices.shift(); //removing the first price 
+console.log(prices); //logging prices

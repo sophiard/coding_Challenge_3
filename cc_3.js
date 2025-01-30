5e89e23f593957afe0f2aa8c2007bb94e9ddbd1a
Sophia Dolan <25srdolan@gmail.com>
2025-01-30 17:41:50 -0500

    Task 1 - Product Price Management: EDITED log to add product prices

--- cc_3.js
+++ cc_3.js
@@ -2,4 +2,7 @@
 let prices = [190,777,678,908,607]; //setting my orginal prices 
 prices.push(808); //adding a price 
 prices.shift(); //removing the first price 
-console.log(prices); //logging prices
+console.log("Product prices:", prices); //logging prices
+
+// task 2: Modifying Customer Orders Scenario: 
+
